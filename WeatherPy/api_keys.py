574982502052311bda35b2243c185d8d1e35503b
git config --global user.name your_username <dallen91801@gmail.com>
2024-09-16 13:54:40 -0700

    Update .gitignore to exclude API keys file

---
+++ WeatherPy/api_keys.py
@@ -0,0 +1,15 @@
+# Geoampify API Key
+geoapify_api = "65fa4c34aefb4a45bf0092ed37bd10b1"
+
+# Openweather API Key
+openweather_api = "cd9eb7f33e8b8e3b11c3aea2be2acd80"
+
+cp /users/board/pythong-api-challenge\WeatherPy\WeatherPy\WeatherPyWeatherPy.ipynb 
+
+"C:\Users\board\python-api-challenge\WeatherPy\WeatherPy\WeatherPy.ipynb"
+
+
+
+
+
+
